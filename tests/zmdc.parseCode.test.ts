7847fd69c52b16d0ce92ce1d1ab904205c49b971
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2025-04-10 12:11:34 +0200

    extended test

--- tests/zmdc.parseCode.test.ts
+++ tests/zmdc.parseCode.test.ts
@@ -3,11 +3,19 @@ import {
     parseCode
 } from "../lib/zmdc.ts";
 
+const outsideHTML = (`
+<div>
+    <span id="outside"></span>
+</div>
+`).trim();
+
 const insideFn = (`
     function demoInsiderFn() {
         // tag: this-is-taken-as-comment
         console.log("Test inside");
-        // <span id="test"></span>
+        // <div>
+        //     <span id="test"></span>
+        // </div>
         document.getElementById("test").innerText = "Insider";
     }
     return demoInsiderFn;
@@ -16,15 +24,18 @@ const insideFn = (`
 const outsideFn = (`
 function demoFunctionInside() {
     // tag: demo-function-inside
-    ${insideFn}    
+    ${insideFn}
+${outsideHTML.split('\n').map(l => '    // ' + l).join('\n')}
 }`).trim()
 
 test("parseCode should take inside function as JavaScript code", () => {
     const example = parseCode(outsideFn.split('\n'));
+    //console.log(outsideFn)
     const expected = (' '.repeat(4) + insideFn).split('\n')
         .map(l => l.slice(4))
         .join('\n');
     console.log(expected)
     expect(example.js).toStrictEqual(expected);
+    expect(example.html).toStrictEqual(outsideHTML);
     expect(example.elId).toStrictEqual('demo-function-inside');
 });
